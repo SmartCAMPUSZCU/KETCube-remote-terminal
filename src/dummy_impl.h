38aa0ebc68743bc681f29444577ac657f985cf0e
MartinUbl <cmaranec@seznam.cz>
2019-05-21 20:02:29 +0200

    Small adjustments after firmware branch rebase

--- src/dummy_impl.h
+++ src/dummy_impl.h
@@ -3,6 +3,11 @@
 /* this file contains dummy implementations and includes to satisfy linkage requirements of
  * symbols we don't really use in remote terminal application */
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 #include "ketCube_terminal.h"
 #include "ketCube_common.h"
 #include "ketCube_cfg.h"
@@ -21,11 +26,20 @@ static void ketCube_terminal_cmd_list(void) {}
 static void ketCube_terminal_cmd_enable(void) {}
 static void ketCube_terminal_cmd_disable(void) {}
 
-void HW_GetUniqueId(uint8_t *id)
+inline void HW_GetUniqueId(uint8_t *id)
 {
 	memset(id, 0, 8);
 }
 
 void ketCube_terminal_UsartPrint(char *format, ...) {}
 
+void ketCube_terminal_CoreSeverityPrintln(int severity, char *format, ...) {}
+
+ketCube_cfg_Error_t ketCube_cfg_SetDefaults(ketCube_cfg_moduleIDs_t id, ketCube_cfg_AllocEEPROM_t target, ketCube_cfg_LenEEPROM_t len) { return KETCUBE_CFG_OK; }
+
 ketCube_batMeas_battery_t ketCube_batMeas_batList[] = { { NULL, NULL, 0, 0 } };
+
+#ifdef __cplusplus
+extern "C"
+}
+#endif
